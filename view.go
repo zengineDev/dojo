f5610d4ca9c2b6dd9e47f44252e79807a7a6eeb3
christian <zengineChris@gmail.com>
2021-05-13 19:59:22 +0200

    fix: the view method now returns an error

--- view.go
+++ view.go
@@ -3,7 +3,6 @@ package dojo
 import (
 	"fmt"
 	"html/template"
-	"log"
 	"path/filepath"
 )
 
@@ -12,22 +11,17 @@ type ViewData map[string]interface{}
 // TODO move this in the app so it is
 var functions = template.FuncMap{}
 
-func (app Application) View(ctx Context, viewName string, data ViewData) {
-
+func (app Application) View(ctx Context, viewName string, data ViewData) error {
 	name := filepath.Base(fmt.Sprintf("%s/%s.gohtml", app.Configuration.View.Path, viewName))
-
 	ts, err := template.New(name).Funcs(functions).ParseFiles(fmt.Sprintf("%s/%s.gohtml", app.Configuration.View.Path, viewName))
 	if err != nil {
-
-		log.Println(err.Error())
-		return
+		return err
 	}
 
 	// Load the other templates
 	ts, err = ts.ParseGlob(filepath.Join(fmt.Sprintf("%s/layouts/*.gohtml", app.Configuration.View.Path)))
 	if err != nil {
-		log.Println(err.Error())
-		return
+		return err
 	}
 
 	// TODO merge all data from the context to the view
@@ -36,6 +30,8 @@ func (app Application) View(ctx Context, viewName string, data ViewData) {
 
 	err = ts.Execute(ctx.Response(), data)
 	if err != nil {
-		log.Println(err.Error())
+		return err
 	}
+
+	return nil
 }
